e6861a318d314bc80ccd4310dc9b04a06b0a06c4
pan-de-hua <pandehua@live.com>
2017-06-23 12:25:36 +0800

    Initial commit

---
+++ src/main/java/one/pdh/com/OsystemApplication.java
@@ -0,0 +1,12 @@
+package one.pdh.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsystemApplication.class, args);
+	}
+}
---
+++ src/main/java/one/pdh/com/model/Role.java
@@ -0,0 +1,7 @@
+package one.pdh.com.model;
+
+/**
+ * Created by pandehua on 2017/6/23.
+ */
+public class Role {
+}
